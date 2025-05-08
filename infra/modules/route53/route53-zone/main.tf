3a37f3371d0ec5d5f592b0022201a4a7c1e3ea29
Wisnu Anggoro <wisnu@anggo.ro>
2025-05-08 16:49:25 +0200

    Create Route53 record

--- infra/modules/route53/route53-zone/main.tf
+++ infra/modules/route53/route53-zone/main.tf
@@ -1,3 +1,19 @@
 resource "aws_route53_zone" "anggoro_net" {
   name = "anggoro.net"
 }
+
+resource "aws_route53_record" "dev" {
+  zone_id = aws_route53_zone.anggoro_net.zone_id
+  name    = "dev.anggoro.net"
+  type    = "A"
+  ttl     = 300
+  records = ["192.0.2.123"]
+}
+
+resource "aws_route53_record" "staging" {
+  zone_id = aws_route53_zone.anggoro_net.zone_id
+  name    = "staging.anggoro.net"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["your-other-target.example.com"]
+}
